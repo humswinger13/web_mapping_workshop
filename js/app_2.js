9bf8977dd91cb7f99a00a4a88260b78f04a4a57e
humswinger13 <terihunsinger@hotmail.com>
2015-02-19 14:59:04 -0600

    Update app_2.js

--- js/app_2.js
+++ js/app_2.js
@@ -2,7 +2,7 @@
 // Enter your mapbox map id here to reference it for the base layer
 
 var mapId = 'humswinger13.l8mm4f1k'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var token = 'pk.eyJ1IjoiaHVtc3dpbmdlcjEzIiwiYSI6ImgtS2RhbkEifQ.lrT6WSaNyhI1-7O-tEQmjg'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
